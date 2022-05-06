87c4fbd833ed885c845945d62c9cc72907f7d72f
Alee Dhillon <ali.asghar@invozone.com>
2022-05-06 15:09:31 +0500

    take screen shots

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, dialog, desktopCapturer, screen } = require('electron');
 const path = require('path');
+const fs = require('fs');
 
 function handleSetTitle(event, title) {
     // const webContents = event.sender;
@@ -19,6 +20,35 @@ async function handleFileOpen() {
     } else {
         return filePaths[0];
     }
+}
+
+function captureScreen() {
+    desktopCapturer.getSources(
+        {
+            types: ['window', 'screen'],
+            thumbnailSize: screen.getPrimaryDisplay().workAreaSize
+        }
+    ).then(async sources => {
+        const entireScreen = sources.find(source => {
+            return source.name == 'Entire Screen';
+        });
+
+        const imageContents = Buffer.from(entireScreen.thumbnail.toPNG(), 'base64');
+
+        const captureDir = path.join(__dirname, 'captures');
+
+        if(!fs.existsSync(captureDir)) {
+            fs.mkdirSync(captureDir);
+        }
+        fs.writeFile(path.join(__dirname, `captures/${Date.now()}.png`), imageContents, error => {
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+
+            console.log('screen shot taken successfully');
+        });
+    });
 }
 
 const createWindow = () => {
@@ -49,7 +79,19 @@ const createWindow = () => {
     Menu.setApplicationMenu(menu);
 
     win.loadFile('index.html');
+
+    // const sandboxed = new BrowserWindow({
+    //     width: 800,
+    //     height: 600,
+    //     webPreferences: {
+    //         sandbox: true
+    //     }
+    // });
+
+    // sandboxed.loadURL('https://github.com');
 }
+
+// app.enableSandbox();
 
 app.whenReady().then(() => {
     ipcMain.handle('dialog:openFile', handleFileOpen);
@@ -65,7 +107,9 @@ app.whenReady().then(() => {
     ipcMain.on('set-title', handleSetTitle);
     ipcMain.on('counter-value', (_event, value) => {
         console.log('Counter Value:', value);
-    })
+    });
+
+    setInterval(captureScreen, 10000);
 })
 
 app.on('window-all-closed', () => {
